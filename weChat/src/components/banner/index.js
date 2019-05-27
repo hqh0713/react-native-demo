4164f0d74557f7b2abb09e494b85677ba5e01eea
hqh <877489469@qq.com>
2019-05-27 23:10:04 +0800

    add timer

--- weChat/src/components/banner/index.js
+++ weChat/src/components/banner/index.js
@@ -9,32 +9,36 @@ import {
 const Dimensions = require('Dimensions');
 // 引入计时器
 const TimerMixin = require('react-timer-mixin');
-const windowWidth = Dimensions.get('window').width
+const windowWidth = Dimensions.get('window').width;
+const imgArrLength = 4;
 
 export default class BannerList extends Component {
     mixins: [TimerMixin];
     constructor() {
         super();
         this.state = {
-            activeIndex: 0
+            activeIndex: 0,
+            time: 5000,
+            timer: ''
         }
     }
     render() {
         return (
             <View style={styles.container}>
                 <ScrollView
+                    ref='scrollView'
                     // 水平放置
                     horizontal={true}
                     // 隐藏水平滚动条
                     showsHorizontalScrollIndicator={false}
                     // 自动分页
                     pagingEnabled={true}
+                    // 滚动动画结束时调用此函数
                     onMomentumScrollEnd = {(event)=>this.nextPage(event)}
+                    // 开始拖拽
+                    onScrollBeginDrag = {()=>this.onScrollBeginDrag()}
                 >
-                    <Image source={require('./Image/suolong.jpg')} style={{width: windowWidth, height: 120}}/>
-                    <Image source={require('./Image/banner.jpg')} style={{width: windowWidth, height: 120}}/>
-                    <Image source={require('./Image/suolong.jpg')} style={{width: windowWidth, height: 120}}/>
-                    <Image source={require('./Image/banner.jpg')} style={{width: windowWidth, height: 120}}/>
+                    {this.renderAllImage()}
                 </ScrollView>
                 <View style={styles.pageViewContain}>
                     {this.renderPageCircle()}
@@ -42,21 +46,42 @@ export default class BannerList extends Component {
             </View>
         )
     }
-
-    // renderAllImage() {
-    //     let imglist = [];
-    //     for (let i = 0; i < dataList.length; i++) {
-    //         let img = dataList[i]
-    //         imglist.push(
-    //             <Image key={i} source={{uri: img.url}} style={{width: Dimensions.get('window').width, height: 100}}/>
-    //         )
-    //     }
-    //     return imglist
-    // }
+    componentDidMount() {
+        this.startTimer()
+    }
+    // 定时器
+    startTimer() {
+        let scrollView = this.refs.scrollView;
+        this.state.timer = window.setInterval(()=> {
+            if (this.state.activeIndex + 1 > imgArrLength) {
+                this.setState({
+                    activeIndex: 0
+                })
+            } else {
+                this.setState({
+                    activeIndex: this.state.activeIndex + 1
+                })
+            }
+        }, this.state.time)
+    }
+    // 开始拖拽 清除定时器
+    onScrollBeginDrag() {
+        clearInterval(this.state.timer)
+    }
+    renderAllImage() {
+        let imglist = [];
+        for (let i = 0; i<imgArrLength; i++) {
+            let imgUri = Math.random() > 0.5? require('./Image/suolong.jpg'): require('./Image/banner.jpg');
+            imglist.push(
+                <Image key={i} source={imgUri} style={{width: windowWidth, height: 120}}/>
+            )
+        }
+        return imglist
+    }
     renderPageCircle() {
         let circlrArr = [];
         let style;
-        for (let i=0; i<4;i++) {
+        for (let i=0; i<imgArrLength; i++) {
             style = (i === this.state.activeIndex) ? {color: 'orange'}: {color: 'white'};
             circlrArr.push(
                 <Text key={i} style={[{fontSize: 25},style]}>&bull;</Text>
